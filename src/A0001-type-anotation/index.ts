c16e49c50ecd2a79e6595c6eec8aea87f8f5ce57
Leandro Paiva <leandroapaiva@yahoo.com>
2022-10-04 14:00:38 -0300

    iniciando typeScript

---
+++ src/A0001-type-anotation/index.ts
@@ -0,0 +1,6 @@
+/*eslint-disable*/
+let nome: string = 'Leandro'; //Qualquer tipo de strings: "" '' ``
+let idade: number = 30; //10, 1.57, -5.55, 0xf00d, 0b1010, 0o7744
+let adulto: boolean = true; //True ou false
+let simbolo: symbol = Symbol('qualquer-symbol'); //Symbol
+//let big: bigint = 10n; //bigint  
